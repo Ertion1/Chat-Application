5f0723424803e39c5b490d5a522cf4c5778322de
Ertion1 <ertionsyziu@gmail.com>
2024-12-04 00:11:14 +0100

    Add files via upload

---
+++ src/test/java/com/example/Chetting/ChettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
